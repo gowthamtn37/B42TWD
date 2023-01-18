59da30c07a764fba95487156034b3c4cc801befc
gowthamtn37 <gowthamtn37@gmail.com>
2023-01-19 04:02:30 +0530

    userList

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./App.css";
 
+let contact = [{name:"Asok",
+pic : "https://th.bing.com/th/id/OIP.UGlKxiZQylR3CnJIXSbFIAHaLL?pid=ImgDet&rs=1" },
+
+              {name:"Bala",
+pic:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg"},
+
+              {name:"Hari",
+              pic:"https://images.unsplash.com/photo-1592009309602-1dde752490ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"}]
+
 export default function App() {
   return (
     <div className="App">
-      <Msg
-        name="Asok"
-        pic="https://th.bing.com/th/id/OIP.UGlKxiZQylR3CnJIXSbFIAHaLL?pid=ImgDet&rs=1"
-      />
-      <Msg
-        name="Bala"
-        pic="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg"
-      />
-      <Msg
-        name="Hari"
-        pic="https://images.unsplash.com/photo-1592009309602-1dde752490ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-      />
+      
+      {/* array of string into array of jsx */}
+      { contact.map((user)=>(<Msg name={user.name} pic={user.pic}
+        
+        />))}
+        
     </div>
   );
 }
